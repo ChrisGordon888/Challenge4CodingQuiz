90175f299ebb5ccb977ced12b3f78b3a05b4bd7f
Chris Gordon <chris.c.gordon888@gmail.com>
2023-07-30 20:55:47 -0500

    updated README.md, CSS, Index.html, and JS.

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 # 04 Web APIs: Code Quiz
+The purpose of this project is to provide a timed coding quiz with multiple-choice questions.
 
-## Your Task
+## Table of Contents
+1. [Task](#task)
+2. [User Story](#user-story)
+3. [Acceptance Criteria](#acceptance-criteria)
+4. [Technologies Used](#technologies-used)
+5. [Installation and Setup](#installation-and-setup)
+6. [Usage](#usage)
+7. [Contribution Guidelines](#contribution-guidelines)
+8. [License](#license)
+9. [Contact](#contact)
 
-At some point in your journey to become a full-stack web developer, you’ll likely be asked to complete a coding assessment&mdash;perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges. 
 
-To help familiarize you with these tests and allow you to use the skills covered in this module, this Challenge invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. 
-
-This week’s coursework will equip you with all the skills you need to succeed in this assignment.
+## Task
+This Challenge required me to build a timed coding quiz with multiple-choice questions. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.
 
 ## User Story
-
-```
 AS A coding boot camp student
 I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
 SO THAT I can gauge my progress compared to my peers
-```
 
 ## Acceptance Criteria
-
-```
 GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
@@ -30,70 +33,23 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score
-```
 
-## Mock-Up
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
 
-The following animation demonstrates the application functionality:
+## Installation and Setup
+Clone the repository from GitHub to your local machine. Open the `index.html` file in a web browser to start the quiz.
 
-![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/04-web-apis-homework-demo.gif)
+## Usage
+This quiz can be used as a training tool for learning JavaScript. It is recommended to take the quiz multiple times in order to fully learn the material.
 
-## Grading Requirements
+## Contribution Guidelines
+If you wish to contribute to this project, please fork the repository and make your changes.
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+## License
+This project is licensed under the MIT License.
 
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
----
-
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+## Contact
+If you have any questions about this project, feel free to contact me on github; my username is ChrisGordon888.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,120 +1,58 @@
-
-
 // DOM elements
-// Get a reference
-const quizEl = document.getElementById("quiz")
-const InitHeader = document.getElementById("InitHeader")
+const quizEl = document.getElementById("quiz");
+const InitHeader = document.getElementById("InitHeader");
 const startBtn = document.getElementById("start-btn");
+const timerEl = document.getElementById("timer");
+const questionTitleEl = document.getElementById("question-title");
+const choicesEl = document.getElementById("choices");
+choicesEl.addEventListener("click", handleChoiceClick);
+
+const questionsEl = document.getElementById("questions");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const duration = 33; 
+let timeRemaining = duration;
+
+let intervalId;
+
 const questions = [
     {
-      question: "An array can be contructed by ___.",
-      choices: ["Array Literal Notation", "Array Constructor with a single parameter", "Array Contructor with multiple paramerters", "All of the above"],
-      answer: "All of the above",
-    },
-    {
-      question: "Which keyboard event object method is not obsolete?",
-      choices: ["KeyboardEvent.initKeyEvent()", "KeyboardEvent.initKeyboardEvent", "KeyboardEvent()", "KeyboardEvent.char"],
-      answer: "KeyboardEvent()",
-    },
-    {
-      question: "The for statement loop repeats unil the specified condition ___.",
-      choices: ["Is True", "Is False", "Is equal to one", "None of the above"],
-      answer: "Is False",
-    },
-    {
-      question: "If the parameter provided for a clearInterval() does not identify a previously established action ___",
-      choices: ["Then, it stops previous setInterval()", "Then, it does nothing", "Then, it continues the repeating action", "Then, it is undefined and breaks our code"],
-      answer: "Then, it does nothing",
-    },
-  ];
+    question: "An array can be contructed by ___.",
+    choices: ["Array Literal Notation", "Array Constructor with a single parameter", "Array Contructor with multiple paramerters", "All of the above"],
+    answer: "All of the above",
+  },
+  {
+    question: "Which keyboard event object method is not obsolete?",
+    choices: ["KeyboardEvent.initKeyEvent()", "KeyboardEvent.initKeyboardEvent", "KeyboardEvent()", "KeyboardEvent.char"],
+    answer: "KeyboardEvent()",
+  },
+  {
+    question: "The for statement loop repeats unil the specified condition ___.",
+    choices: ["Is True", "Is False", "Is equal to one", "None of the above"],
+    answer: "Is False",
+  },
+  {
+    question: "If the parameter provided for a clearInterval() does not identify a previously established action ___",
+    choices: ["Then, it stops previous setInterval()", "Then, it does nothing", "Then, it continues the repeating action", "Then, it is undefined and breaks our code"],
+    answer: "Then, it does nothing",
+  },
+];
 
-if (Array.isArray(questions)) {
-  // Make sure the variable has the expected structure
-  if (questions.length > 0) {
-    console.log("The array has elements");
-  } else {
-    console.log("The array is empty");
-  }
-} else {
-  console.log("The variable is not an array");
-}
-
-// Get the current question object
-const questionTitleEl = document.getElementById("question-title")
-const questionsEl = document.getElementById("questions")
-const choicesEl = document.getElementById("choices")
-
-// Get a reference to the timer element
-const timerEl = document.getElementById("timer");
-const duration = 33
-// Set the initial time remaining to 33 seconds
-let timeRemaining = duration;
-// Function to update the timer display
 function updateTimerDisplay() {
   timerEl.textContent = timeRemaining;
 }
 
-let intervalId;
-
-// Function to end the game
-function endGame() {
-  console.log("Game over!");
-  // Clear the timer interval
-  clearInterval(intervalId);
-  // Display the score or do something else to end the game
-  // For example:
-  const scoreEl = document.getElementById("highscores");
-  scoreEl.textContent = "You scored X out of Y";
-}
-
-// Function to start the timer countdown
-function startTimer() {
-  // Call the updateTimerDisplay function to set the initial time display
-  updateTimerDisplay();
-  // Decrement the time remaining every second using setInterval
-  intervalId = setInterval(() => {
-    timeRemaining--;
-    updateTimerDisplay();
-    // Check if the time has run out
-    if (timeRemaining <= 0) {
-      endGame();
-    }
-  }, 1000);
-}
-
-// Add a click event listener to the start button
-startBtn.addEventListener("click", () => {
-  // Start the timer countdown
-  startTimer();
-  // Hide the start button and InitHeader and show the quiz container
-  InitHeader.classList.add("hidden");
-  startBtn.classList.add("hidden");
-  // Show the first question
-  showQuestion(0)
-  questionsEl.classList.remove("hidden"); 
-});
-
-
-// Function to show a question and its choices
-function showQuestion(index) {
-  // Set the question title and choices using innerHTML
-  questionTitleEl.innerHTML = questions[index].question;
-  choicesEl.innerHTML = `
-    <li><button class="choice">${questions[index].choices[0]}</button></li>
-    <li><button class="choice">${questions[index].choices[1]}</button></li>
-    <li><button class="choice">${questions[index].choices[2]}</button></li>
-    <li><button class="choice">${questions[index].choices[3]}</button></li>
-  `;
-  // Set up an event listener for user input
-  choicesEl.addEventListener("click", handleChoiceClick);
-  // Function to handle user input on a choice button
-  function handleChoiceClick(event) {
-    const choice = event.target.textContent;
-    choicesEl.removeEventListener("click", handleChoiceClick);
+function handleChoiceClick(event) {
+  if(event.target.nodeName === 'BUTTON') {
+    let choice = event.target.textContent;
     if (choice === questions[currentQuestionIndex].answer) {
       console.log("Correct!");
+      score++;
     } else {
       console.log("Incorrect!");
+      timeRemaining -= 5;
     }
     currentQuestionIndex++;
     if (currentQuestionIndex < questions.length) {
@@ -122,27 +60,106 @@ function showQuestion(index) {
     } else {
       endGame();
     }
-    } if (index < questions.length - 1) {
-    // Show the next question after a brief delay
-    setTimeout(() => {
-      showQuestion(index + 1);
-    }, 1000);
-    } else {
-    // End the game after a brief delay
-    setTimeout(() => {
-      endGame();
-    }, 1000);
   }
 }
 
+function endGame() {
+  clearInterval(intervalId);
+  const scoreEl = document.getElementById("highscores");
+  scoreEl.classList.remove('hidden');
+  scoreEl.querySelector('h2').textContent = `You scored ${score} out of ${questions.length}`;
 
+  const submitButton = document.getElementById("submit-score");
+  const initialsInput = document.getElementById("initials");
 
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+    highscores.push({ initials: initialsInput.value, score });
+    localStorage.setItem('highscores', JSON.stringify(highscores));
+    displayHighscores();
+  });
+}
 
+function startTimer() {
+  updateTimerDisplay();
+  intervalId = setInterval(() => {
+    timeRemaining--;
+    updateTimerDisplay();
+    if (timeRemaining <= 0) {
+      endGame();
+    }
+  }, 1000);
+}
 
+startBtn.addEventListener("click", () => {
+  startTimer();
+  InitHeader.classList.add("hidden");
+  showQuestion(currentQuestionIndex);
+  quizEl.classList.remove("hidden");
+  questionsEl.classList.remove("hidden");
+});
 
+function showQuestion(index) {
+  questionTitleEl.innerHTML = questions[index].question;
+  choicesEl.innerHTML = '';
+  for(let i = 0; i < questions[index].choices.length; i++) {
+    choicesEl.innerHTML += `<li><button class="choice">${questions[index].choices[i]}</button></li>`;
+  }
+}
 
+function displayHighscores() {
+  const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+  const highscoreListEl = document.getElementById('highscore-list');
+  highscoreListEl.innerHTML = '';
+  if(highscores.length > 0) {
+    highscores.forEach((highscore, index) => {
+      highscoreListEl.innerHTML += `<li>${index+1}. ${highscore.initials} - ${highscore.score}</li>`;
+    });
+    document.getElementById('clear-highscores').classList.remove('hidden'); // Show the clear highscores button
+  } else {
+    highscoreListEl.innerHTML = "<li>No highscores yet.</li>";
+    document.getElementById('clear-highscores').classList.add('hidden'); // Hide the clear highscores button
+  }
+  document.getElementById('view-highscores').classList.add('hidden');
+  document.getElementById('highscores').classList.remove('hidden');
+  document.getElementById('go-back').classList.remove('hidden');
+}
 
+// Event Listener for clearing highscores
+document.getElementById('clear-highscores').addEventListener('click', () => {
+  localStorage.removeItem('highscores');
+  displayHighscores();
+});
 
+document.getElementById('view-highscores').addEventListener('click', (event) => {
+  event.preventDefault();
+  let highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+  if (highscores.length > 0) {  // show the highscore list only if there are high scores
+    quizEl.classList.add('hidden'); // Hide the quiz container
+    InitHeader.classList.add("hidden");
+    startBtn.classList.add("hidden");
+    document.getElementById('highscores').classList.remove('hidden'); // Show the highscores
+    document.getElementById('view-highscores').classList.add('hidden');
+    document.getElementById('go-back').classList.remove('hidden');
+    document.getElementById('clear-highscores').classList.remove('hidden');
+    displayHighscores();
+  }
+});
+
+// Event Listener for going back to the quiz
+document.getElementById('go-back').addEventListener('click', () => {
+  document.getElementById('view-highscores').classList.remove('hidden');
+  document.getElementById('highscores').classList.add('hidden');
+  document.getElementById('go-back').classList.add('hidden');
+  document.getElementById('clear-highscores').classList.add('hidden');
+  InitHeader.classList.remove("hidden");
+  startBtn.classList.remove("hidden");
+  document.getElementById("initials").value = '';
+  currentQuestionIndex = 0;  // reset the question index
+  score = 0;  // reset the score
+  timeRemaining = duration;  // reset the time
+});
 
 
 
